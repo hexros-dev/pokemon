1184ad2a6123e0cf8ca53c5af7e30c2f20ce125e
Hexros <hexros.dev@gmail.com>
2024-07-31 04:58:18 +0700

    Update image-pokemon-names.userscript.js

--- image-pokemon-names.userscript.js
+++ image-pokemon-names.userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Image Pokémon Names
 // @namespace    https://example.com/
-// @version      1.2.5
+// @version      1.2.6
 // @description  Hiển thị hình ảnh trong name Pokémon
 // @author       Hexros Raymond
 // @match        *://*/truyen/*/*/*/
@@ -53,8 +53,8 @@
         styleItem.forEach(element => {
             element.style.display = 'inline-block';
             element.style.margin = '-5px 0 0 2px';
-            element.style.width = '25px';
-            element.style.height = '25px';
+            element.style.width = '35px';
+            element.style.height = '35px';
         });
     }
 
